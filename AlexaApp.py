f60eccd7dd696613e36da30d4fabc7c25ad3082e
gersonbarajas62 <gersonbarajas62@gmail.com>
2023-02-07 20:11:11 -0600

    first commads

--- AlexaApp.py
+++ AlexaApp.py
@@ -1,1 +1,19 @@
-print ("my first python app")
+#importing Libraries
+import speech_recognition as sr
+import pyttsx3 
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+engine.say("I am your bot")
+engine.say("how can i help you")
+engine.runAndWait()
+try:
+    with sr.Microphone() as source:
+        print("start speaking")
+        voice = listener.listener(source)
+        command = listener.recognize_google(voice)
+        command = command.lower()
+        if 'alexa' in command:
+            print (command)
+except:
+    pass 
